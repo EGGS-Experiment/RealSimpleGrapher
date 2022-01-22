65f79c5a90fd52c0a7ba527920c69c5ca633ed2d
Clayton Ho <claytonho@g.ucla.edu>
2022-01-21 18:03:34 -0800

    fixed dataset array allocation problem which prevented tracking of datasets
    dataset objects now create a dataset upon initialization to prevent having to check in getData

--- Dataset.py
+++ Dataset.py
@@ -2,6 +2,11 @@ import numpy as np
 from PyQt5.QtCore import QObject
 from twisted.internet.defer import inlineCallbacks, returnValue, DeferredLock
 
+from datetime import datetime
+
+def forceEvaluate(callback, func, *args):
+    func(*args)
+    return callback
 
 class Dataset(QObject):
     '''
@@ -10,30 +15,40 @@ class Dataset(QObject):
 
     def __init__(self, data_vault, context, dataset_location, reactor):
         super(Dataset, self).__init__()
-        self.data = None
         self.accessingData = DeferredLock()
         self.reactor = reactor
-        self.dataset_location = dataset_location
-        self.data_vault = data_vault
-        self.updateCounter = 0
         self.context = context
-        self.connectDataVault()
-        self.setupListeners()
 
         # dataset storage variables
+        self.dataset_location = dataset_location
+        self.data_vault = data_vault
+        self.data = None
         self.points_per_grab = 1000
         self.last_index = 0
-
+        self.updateCounter = 0
+        
         # startup sequence
         self.connectDataVault()
+        self.openDataset()
         self.setupListeners()
 
 
     # SETUP CONNECTION
     @inlineCallbacks
     def connectDataVault(self):
+        yield self.accessingData.acquire()
         yield self.data_vault.cd(self.dataset_location[0], context=self.context)
         _, self.dataset_name = yield self.data_vault.open(self.dataset_location[1], context=self.context)
+    
+    @inlineCallbacks
+    def openDataset(self):
+        # open the dataset
+        yield self.data_vault.cd(self.dataset_location[0], context=self.context)
+        yield self.data_vault.open(self.dataset_location[1], context=self.context)
+        # allocate array size based on dataset size
+        dataset_shape = yield self.data_vault.shape(context=self.context)
+        self.data = np.zeros(dataset_shape)
+        self.accessingData.release()
 
     @inlineCallbacks
     def setupListeners(self):
@@ -43,11 +58,6 @@ class Dataset(QObject):
     @inlineCallbacks
     def disconnectDataSignal(self):
         yield self.data_vault.removeListener(listener=self.updateData, source=None, ID=11111, context=self.context)
-
-    @inlineCallbacks
-    def openDataset(self):
-        yield self.data_vault.cd(self.dataset_location[0], context=self.context)
-        yield self.data_vault.open(self.dataset_location[1], context=self.context)
 
 
     # GETTERS
@@ -62,14 +72,15 @@ class Dataset(QObject):
 
     @inlineCallbacks
     def getLabels(self):
+        yield self.accessingData.acquire()
         labels = []
-        yield self.openDataset()
         _, all_dep = yield self.data_vault.variables(context=self.context)
         for i in range(len(all_dep)):
             label_tmp = all_dep[i][0] + ' - ' + self.dataset_name
             if label_tmp in labels:
                 label_tmp += ' (' + str(i) + ')'
             labels.append(label_tmp)
+        self.accessingData.release()
         returnValue(labels)
 
     def updateData(self, c, msg):
@@ -87,16 +98,13 @@ class Dataset(QObject):
         # get data from the datavault
         Data = yield self.data_vault.get(self.points_per_grab, context=self.context)
         Data = np.array(Data)
-        rows = np.shape(Data)[0]
-        # add data to dataset
-        if self.data is not None:
-            self.data[self.last_index: self.last_index + rows] = Data
-            self.last_index += rows
-        # create new dataset
+        next_index = self.last_index + np.shape(Data)[0]
+        # add to array if we have the space
+        if next_index <= np.shape(self.data)[0]:
+            self.data[self.last_index: next_index] = Data
+        # otherwise append to array
         else:
-            dataset_shape = yield self.data_vault.shape(context=self.context)
-            self.data = np.zeros(dataset_shape)
-            self.data[self.last_index: self.last_index + rows] = Data
-            self.last_index += rows
+            self.data = np.append(self.data, Data, axis=0)
+        self.last_index = next_index
         # release communication
         self.accessingData.release()
