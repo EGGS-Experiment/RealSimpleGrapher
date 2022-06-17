63b0a3d17fb68259302b49c444a2ac990cbe663e
Clayton Ho <claytonho@g.ucla.edu>
2022-06-17 14:58:12 -0700

    removed labrad units since they're unreasonable

--- analysis/rabi/lamb_dicke.py
+++ analysis/rabi/lamb_dicke.py
@@ -5,7 +5,7 @@ from labrad import units as U, types as T
 class lamb_dicke(object):
 
     @classmethod
-    def lamb_dicke(self, trap_frequency, projection_angle, laser_wavelength=T.Value(729, 'nm'), amumass=40):
+    def lamb_dicke(self, trap_frequency, projection_angle, laser_wavelength=729e-9, amumass=40):
         '''
         Computes the lamb-dicke parameter.
         @var theta: angle between the laser and the mode of motion. 90 degrees is perpendicular
@@ -18,12 +18,12 @@ class lamb_dicke(object):
         mass = amumass * U.amu
         k = 2. * pi / laser_wavelength
         eta = k * (U.hbar / (2 * mass * 2 * pi * frequency)) ** .5 * abs(cos(theta * 2. * pi / 360.0))
-        eta = eta.inBaseUnits().value
+        #eta = eta.inBaseUnits().value
         return eta
 
 
 if __name__ == '__main__':
-    trap_frequency = T.Value(3.0, 'MHz')
+    trap_frequency = 30e6
     projection_angle = 45  # degrees
     eta = lamb_dicke.lamb_dicke(trap_frequency, projection_angle)
     print('eta {}'.format(eta))
